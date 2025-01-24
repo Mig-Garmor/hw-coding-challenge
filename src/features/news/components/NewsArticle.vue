58dbb604a8dd8fb007fbbe5be287436d94cfde32
Mig-Garmor <thecodermiguel@gmail.com>
2025-01-24 20:18:12 +0100

    Relocated NewsArticle.vue

--- src/features/news/components/NewsArticle.vue
+++ src/features/news/components/NewsArticle.vue
@@ -3,7 +3,9 @@
     <h2 class="text-xl font-bold">{{ article.title }}</h2>
     <p>{{ article.description }}</p>
     <p class="text-gray-500">Published: {{ article.pubDate }}</p>
-    <a :href="article.link" target="_blank" class="text-blue-500">Read More</a>
+    <a :href="article.link"
+       target="_blank"
+       class="text-blue-500">Read More</a>
   </div>
 </template>
 
