14173309a661aa2ddf3e25f0c04792ade3f0bd54
Mig-Garmor <thecodermiguel@gmail.com>
2025-01-24 20:22:18 +0100

    Adjusted tailwind import after relocation to assets/styles folder

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./tailwind.css";
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/styles/tailwind.css';
 
 // Import Vue Toastify
-import Vue3Toastify from "vue3-toastify";
+import Vue3Toastify from 'vue3-toastify';
 
-import "vue3-toastify/dist/index.css"; // Import the CSS for Toastify
+import 'vue3-toastify/dist/index.css'; // Import the CSS for Toastify
 
 createApp(App)
   .use(Vue3Toastify, {
     autoClose: 2000,
   })
-  .mount("#app");
+  .mount('#app');
